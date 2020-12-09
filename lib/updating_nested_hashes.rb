8eeb8e1030185bd677b6d0c8a24588b9a8e77e08
Christopher Read <christopherread22@outlook.com>
2020-12-09 23:34:52 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -52,11 +52,12 @@ def add_characters
          ]
       }
    }
-epic_tragedy[:additional_characters] = [
+epic_tragedy[:additional_characters] = []
+
   prince = { name: "Prince Escalus" }
   apothecary = { name: "Apothecary" }
-  {name: "Prince Escalus" },
-  {name: "Apothecary" } 
-  ]
+  
+  epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+  epic_tragedy[:additional_characters] << { name: "Apothecary" }
   
 end
